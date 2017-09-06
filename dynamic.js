01c56e680886ba1cb28b8d67a671c89f93d4cf20
ELIZABETH MIN <elmm682@gmail.com>
2017-09-05 21:18:05 -0700

    replace random color w/ rainbow

--- dynamic.js
+++ dynamic.js
@@ -1,57 +1,46 @@
-var color;
+var color = "#ff0000";
 
 function main() {
-  //while (true) {
+  //while (true) { commented to avoid a forever loop before ready
     newColor();
     // change backlit glow to Hello World
   //}
 }
 
 function newColor() {
-  // generate a random hex color value
-  color = "";
-  for (i = 1; i <= 6; i++) {
-    var number = Math.floor(Math.random() * 16);
-    if (number <= 9) {
-      color += number;
-    } else {
-      switch (number) {
-        case 10:
-          color += "A";
-          break;
-        case 11:
-          color += "B";
-          break;
-        case 12:
-          color += "C";
-          break;
-        case 13:
-          color += "D";
-          break;
-        case 14:
-          color += "E";
-          break;
-        case 15:
-          color += "F";
-          break;
-        default:
-          console.log("Hex Generation Error");
-          break;
-      }
-    }
-  }
+  // generate the next color in the rainbow
+  switch (color) {
+    case "#ff0000":
+      color = "#ff8000";
+      break;
+    case "#ff8000":
+      color = "#ffff00";
+      break;
+    case "#ffff00":
+      color = "#00ff00";
+      break;
+    case "#00ff00":
+      color = "#00ffff";
+      break;
+    case "00ffff":
+      color = "0000ff";
+      break;
+    case "0000ff":
+      color = "8000ff";
+      break;
+    case "#8000ff":
+      color = "#ff00ff";
+      break;
+    case "#ff00ff":
+      color = "#ff0000";
+      break;
+    default:
+      console.log("Rainbow Generation Error");
   console.log(color);
+  }
 }
 
 //// TweenMax functions
 function colorTransition() {
   // TweenMax functionality
-}
-
-//// TweenMax functions
-function goIn(arrayValues) {
-  TweenMax.to(arrayValues, 1, {
-    width: "20%",
-    opacity: 0.2,
-  })
 }
